e397f86e4aeeddc34ea8ee274b7bd210db4559fc
Magnus <104272224+Magnus-Mage@users.noreply.github.com>
2025-05-13 22:19:21 +0530

    JSR

--- main.cpp
+++ main.cpp
@@ -40,6 +40,15 @@ struct Mem
     // asert here address is < MAX_MEM
     return Data[Address];
   }
+  
+  // Read the least significant byte off the memory
+  // Write two bytes
+  void WriteWord(Word Value, u32 Address, u32& Cycles)
+  {
+    Data[Address]     = Value & 0xFF;
+    Data[Address + 1] = (Value >> 8);
+    Cycles -= 2;
+  }
 };
 
 // CPU logic
@@ -78,6 +87,24 @@ struct CPU
     return Data;
   }
 
+  Word FetchWord(u32& Cycles, Mem& memory)
+  {
+    // 6502 is little endian
+    Word Data = memory[PC];
+    PC++;
+
+    Data |= (memory[PC] << 8);
+    PC++;
+    Cycles -= 2;
+
+    // if you want to handle endian
+    // you would have to swap bytes here 
+    // if (PLATFORM_BIG_ENDIAN)
+    // SwapByteInWord(Data)
+
+    return Data;
+  }
+
   Byte ReadByte(u32& Cycles, Byte Address, Mem& memory)
   {
     Byte Data = memory[Address];
@@ -88,8 +115,10 @@ struct CPU
   // operation codes 
   // thsi one specifically to get address mode instruction
   static constexpr Byte
-    INS_LDA_IM = 0xA9,
-    INS_LDA_ZP = 0xA5;
+    INS_LDA_IM  = 0xA9,
+    INS_LDA_ZP  = 0xA5,
+    INS_LDA_ZPX = 0xB5,
+    INS_JSR     = 0x20;
  
   void LDASetStatus()
   {
@@ -121,6 +150,35 @@ struct CPU
             LDASetStatus();
           }
         break;
+        case INS_LDA_ZPX:
+          {
+            Byte ZeroPageAddrX = 
+              FetchByte(Cycles, memory);
+            ZeroPageAddrX += X;
+            Cycles--;
+            A = ReadByte(Cycles, ZeroPageAddrX, memory);
+            LDASetStatus();
+          }
+        break;
+        case INS_JSR:
+          {
+            Word SubAddr = FetchWord(Cycles, memory);
+            Word ReturnAddr = PC - 1;
+
+            // Push high byte
+            memory[0x0100 + SP] = (ReturnAddr >> 8);
+            SP--;
+            Cycles--;
+
+            // Push low byte
+            memory[0x0100 + SP] = ReturnAddr & 0xFF;
+            SP--;
+            Cycles--;
+
+            PC = SubAddr;
+            Cycles--;
+          }
+        break;
         default:
           {
             printf("Instruction not handled %d", Ins); 
@@ -143,11 +201,22 @@ int main ()
   cpu.Reset(mem);
   
   // start - inline a little program 
-  mem[0xFFFC] = CPU::INS_LDA_ZP;
-  mem[0xFFFC] = 0x42;
-  mem[0x0042] = 0x84;
+  mem[0xFFFC] = CPU::INS_JSR;
+  mem[0xFFFD] = 0x42;
+  mem[0xFFFE] = 0x42;
+  mem[0x4242] = CPU::INS_LDA_IM;
+  mem[0x4243] = 0x84;
   // end - inline a little program
-  cpu.Execute(2 , mem);
+  printf("Memory at 0xFFFC: %d (JSR opcode)\n", mem[0xFFFC]);
+  printf("Memory at 0xFFFD: %d (low byte of address)\n", mem[0xFFFD]);
+  printf("Memory at 0xFFFE: %d (high byte of address)\n", mem[0xFFFE]);
+  printf("Memory at 0x4242: %d (LDA opcode)\n", mem[0x4242]);
+  printf("Memory at 0x4243: %d (immediate value)\n", mem[0x4243]);
+  
+
+  cpu.Execute(8 , mem);
+
+  printf("After execution, A register: %d\n", cpu.A);
 
   return 0;
 }

d09ca3bd496ff23d165319f52558511f3cfec3ac
Magnus <104272224+Magnus-Mage@users.noreply.github.com>
2025-05-31 20:35:48 +0530

    Update Tests

--- tests/test_cpu.cpp
+++ tests/test_cpu.cpp
@@ -20,21 +20,40 @@ public:
 };
 
 
-TEST_F(M6502Test1, RunALittleInlineProgram)
+TEST_F(M6502Test1, LDAImmediateCanLoadAValueIntoTheRegister)
 {
   // start - inline a little program 
-  mem[0xFFFC] = CPU::INS_JSR;
-  mem[0xFFFD] = 0x42;
-  mem[0xFFFE] = 0x42;
-  mem[0x4242] = CPU::INS_LDA_IM;
-  mem[0x4243] = 0x84;
+  mem[0xFFFC] = CPU::INS_LDA_IM;
+  mem[0xFFFD] = 0x84;
   // end - inline a little program
-  cpu.Execute(8 , mem);
+  
+  // when:
+  cpu.Execute(2 , mem);
 
-  EXPECT_EQ(cpu.A, 0x86);
-
+  // then:
+  EXPECT_EQ(cpu.A, 0x84);
+  
+  
   printf("After execution, A register: %d\n", cpu.A);
 
 }
 
 
+TEST_F(M6502Test1, LDAZeroPageCanLoadAValueIntoTheRegister)
+{
+  // start - inline a little program 
+  mem[0xFFFC] = CPU::INS_LDA_ZP;
+  mem[0xFFFD] = 0x42;
+  mem[0x0042] = 0x37;
+  // end - inline a little program
+  
+  // when:
+  cpu.Execute(3 , mem);
+
+  // then:
+  EXPECT_EQ(cpu.A, 0x37);
+  
+  
+  printf("After execution, A register: %d\n", cpu.A);
+
+}
